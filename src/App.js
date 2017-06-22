6580f51dd76664f2536e4cd35f7e1f24f79028ba
Ana Maria Sosa <anamariasosam@gmail.com>
2017-06-21 20:46:46 -0500

    add lyrics from api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container, Header } from 'semantic-ui-react';
+import $ from 'jquery';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      lyric: "",
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: 'http://api.musixmatch.com/ws/1.1/track.lyrics.get',
+      dataType: 'jsonp',
+      contentType: 'application/json',
+      type: 'GET',
+      jsonpCallback: 'jsonp_callback',
+      data: {
+      track_id: '130727382',
+      apikey: 'c4caa5ba22a90e53f85590fdecf2347d',
+      format:"jsonp",
+      callback:"jsonp_callback"
+    },
+    success: (res) => {
+      const index = res.message.body.lyrics.lyrics_body.indexOf(".") + 3;
+      const lyric = res.message.body.lyrics.lyrics_body.slice(0, index);
+      this.setState({ lyric });
+    },
+    error: (data) => console.log(data)
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <Container text className="Lyric_container" textAlign='center'>
+      <Header as='h1'>Header</Header>
+      <p className="Lyric_text">{ this.state.lyric }</p>
+    </Container>
     );
   }
 }
